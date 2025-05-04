82e03591fce6ba8403cc6ca450d8e3381d8640cf
Hwang Duckbae <87626940+Neronem@users.noreply.github.com>
2025-05-05 04:07:14 +0900

    fix : 첫구동시엔 UI 비활성화

--- Assets/Scripts/MetaVerse/UI/ReturnShowUI.cs
+++ Assets/Scripts/MetaVerse/UI/ReturnShowUI.cs
@@ -9,42 +9,48 @@ public class ReturnShowUI : MonoBehaviour
     public GameObject UIOfReturn;
     
     public TextMeshProUGUI BestScoreText;
-    public TextMeshProUGUI TalkAboutYOu;
-
-    private bool isPlayedBefore = false;
+    public TextMeshProUGUI TalkAboutYou;
+    
+    private bool TurnUION = false;
+    private static bool isFirstCamed = true;
     
     // Start is called before the first frame update
     void Start()
     {
         UIOfReturn.SetActive(false);
-        
-        bestScore = PlayerPrefs.GetFloat("BestScore", 0);
-        if (bestScore != 0)
+
+        if (!isFirstCamed)
         {
-            isPlayedBefore = true;
-            BestScoreText.text = bestScore.ToString("F1");
-            if (bestScore < 200f)
+            bestScore = PlayerPrefs.GetFloat("BestScore", 0);
+            if (bestScore != 0)
             {
-                TalkAboutYOu.text = "200점정도면 꽤 멋있을거에요.";
+                TurnUION = true;
+                BestScoreText.text = bestScore.ToString("F1");
+                if (bestScore < 200f)
+                {
+                    TalkAboutYou.text = "200점정도면 꽤 멋있을거에요.";
+                }
+                else if (bestScore >= 200f)
+                {
+                    TalkAboutYou.text = "꽤 실력자군요.";
+                }
+
+                UIOfReturn.SetActive(true);
             }
-            else if (bestScore >= 200f)
-            {
-                TalkAboutYOu.text = "꽤 실력자군요.";
-            }
-            UIOfReturn.SetActive(true);
+        }
+        else
+        {
+            isFirstCamed = false;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isPlayedBefore)
+        if (Input.GetKeyDown(KeyCode.Return) && TurnUION)
         {
-            if (Input.GetKeyDown(KeyCode.Return))
-            {
-                UIOfReturn.SetActive(false);
-                isPlayedBefore = false;
-            }
+            UIOfReturn.SetActive(false);
+            TurnUION = false;
         }
     }
 }
