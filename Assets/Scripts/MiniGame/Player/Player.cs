8a4dd9ee43d659257bffcd2d738e1227ec4103c2
Hwang Duckbae <87626940+Neronem@users.noreply.github.com>
2025-05-04 18:43:19 +0900

    Add : 플레이어 점프 구현

--- Assets/Scripts/MiniGame/Player/Player.cs
+++ Assets/Scripts/MiniGame/Player/Player.cs
@@ -11,7 +11,12 @@ public class Player : MonoBehaviour
     public float startSpeed = 2f;
     public float moveSpeed = 0.2f;
     public float currentSpeed;
+
+    public float jumpForce = 7f;
+    private int jumpCount = 0;
+    public int maxJumpCount = 2;
     
+    private bool isGround = false;
     private void Start()
     {
         animator = GetComponentInChildren<Animator>();
@@ -23,5 +28,36 @@ public class Player : MonoBehaviour
     {
         currentSpeed += Time.deltaTime * moveSpeed;
         rigidbody.velocity = new Vector2(currentSpeed, rigidbody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < maxJumpCount)
+        {
+            // velocity y값초기화 일부러 안하기 (더블점프할때는 그전에 내려오던 값 + 점프력으로 조작감 up)
+            rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); //forceMode2D. Impulse는 즉시 한번 세게 가한다는뜻 (점프에 좋다고 함)
+            if (jumpCount == 1)
+            {
+                animator.SetBool("IsDoubleJump", true);
+            }
+            jumpCount++;
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            animator.SetBool("IsDoubleJump", false);
+            isGround = true;
+            jumpCount = 0;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = false;
+        }
     }
 }
+
